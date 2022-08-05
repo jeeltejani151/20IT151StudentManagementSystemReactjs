a8f76b348b33517c1e9bbd2ac9fd06fcd46092fa
Jeel Tejani <20it151@charusat.edu.in>
2022-08-05 14:13:57 +0530

    Addstudent commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import AddStudent from "./Components/AddStudent";
+import NavBar from "./Components/NavBar";
+import ListStudent from "./Components/ListStudent";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container>
+        <Router>
+          <NavBar/>
+            <AddStudent/>
+          <Routes>
+            <Route path="/student" element={<AddStudent/>} />
+            <Route path="/listStudents" element={<ListStudent/>} />
+          </Routes>
+        </Router>
+      </Container>
   );
 }
 
